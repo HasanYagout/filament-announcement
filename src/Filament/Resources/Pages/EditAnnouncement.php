5a923f4e3c46934654d6f2f2912acdcb4e5ee17c
Hasan Yagout <yagouthasan3@gmail.com>
2026-06-10 10:02:13 +0300

    first commit

--- src/Filament/Resources/Pages/EditAnnouncement.php
+++ src/Filament/Resources/Pages/EditAnnouncement.php
@@ -9,7 +9,24 @@ class EditAnnouncement extends EditRecord
 {
     protected static string $resource = AnnouncementResource::class;
 
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
 
+
+        // Load existing recipients for this announcement
+        $recipients = $this->record->recipients;
+
+        if ($recipients->isNotEmpty()) {
+            // Assume all recipients have the same type (as per your saving logic)
+            $data['recipient_type'] = $recipients->first()->recipient_type;
+            $data['recipient_ids'] = $recipients->pluck('recipient_id')->toArray();
+        } else {
+            $data['recipient_ids'] = [];
+            $data['recipient_type'] = null;
+        }
+
+        return $data;
+    }
     protected function afterSave(): void
     {
         $this->record->recipients()->delete();
